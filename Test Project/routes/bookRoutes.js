95f4c35349e0e1d5c3a11f493313bd5b04b378e7
Ashtinh Le <ashtinhle@gmail.com>
2015-10-08 13:31:05 -0600

    able to patch an entry in the database;

--- Test Project/routes/bookRoutes.js
+++ Test Project/routes/bookRoutes.js
@@ -43,12 +43,36 @@ var routes = function(Book) {
             res.json(req.book);
         })
         .put(function(req, res) {
-                    req.book.title = req.body.title;
-                    req.book.author = req.body.author;
-                    req.book.genre = req.body.genre;
-                    req.book.read = req.body.read;
-                    req.book.save();
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
                     res.json(req.book);
+                }
+
+            });
+        })
+        .patch(function(req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            for (var att in req.body) {
+                req.book[att] = req.body[att];
+            }
+
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+
+            });
         });
     return bookRouter
 };
