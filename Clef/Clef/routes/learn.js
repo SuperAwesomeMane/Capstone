a14e89c718a6b0cb5eb8e960e4815c4fa77d9ab6
Ashtinh Le <ashtinhle@gmail.com>
2015-11-09 21:03:29 -0700

    created login/register html pages, and template for each lesson page

--- Clef/Clef/routes/learn.js
+++ Clef/Clef/routes/learn.js
@@ -9,4 +9,56 @@ router.get('/', function(req, res, next) {
     }
 });
 
+// Lesson routes //========================================
+
+router.get('/lesson1', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson1', {user: req.user});
+	}
+});
+router.get('/lesson2', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson2', {user: req.user});
+	}
+});
+router.get('/lesson3', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson3', {user: req.user});
+	}
+});
+router.get('/lesson4', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson4', {user: req.user});
+	}
+});
+router.get('/lesson5', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson5', {user: req.user});
+	}
+});
+router.get('/lesson6', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson6', {user: req.user});
+	}
+});
+router.get('/lesson7', function(req, res) {
+	if(!req.user) {
+		res.redirect('/login');
+	} else {	
+		res.render('lessons/lesson7', {user: req.user});
+	}
+});
+
 module.exports = router;
