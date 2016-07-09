39fd59847ea33334659ab51dcd1483786ebef89e
Ashtinh Le <ashtinhle@gmail.com>
2016-07-08 18:42:20 -0600

    creating default user to view lessons

--- routes/learn.js
+++ routes/learn.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('./models/userModel');
+
+var defUser = new User() {
+	username: 'User',
+	password: '12345',
+	image: '',
+	email: 'user@user.com',
+	lessonCompleted: 0,
+	facebook: '',
+	google: '',
+	songs: [];
+};
 
 router.get('/', function(req, res, next) {
 	if (req.user) {
-        res.render('learn', {user: req.user});
+        res.render('learn', {user: defUser});
     } else {
         res.render('learn', {user: "null"});
     }
@@ -15,52 +27,52 @@ router.get('/lesson1', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson1', {user: req.user});
+		res.render('lessons/lesson1', {user: defUser});
 	// }
 });
 router.get('/lesson2', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson2', {user: req.user});
+		res.render('lessons/lesson2', {user: defUser});
 	// }
 });
 router.get('/lesson3', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson3', {user: req.user});
+		res.render('lessons/lesson3', {user: defUser});
 	// }
 });
 router.get('/lesson4', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson4', {user: req.user});
+		res.render('lessons/lesson4', {user: defUser});
 	// }
 });
 router.get('/lesson5', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson5', {user: req.user});
+		res.render('lessons/lesson5', {user: defUser});
 	// }
 });
 router.get('/lesson6', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson6', {user: req.user});
+		res.render('lessons/lesson6', {user: defUser});
 	// }
 });
 router.get('/lesson6b', function(req, res) {
-		res.render('lessons/lesson6b', {user: req.user});
+		res.render('lessons/lesson6b', {user: defUser});
 });
 router.get('/lesson7', function(req, res) {
 	// if(!req.user) {
 	// 	res.redirect('/login');
 	// } else {	
-		res.render('lessons/lesson7', {user: req.user});
+		res.render('lessons/lesson7', {user: defUser});
 	// }
 });
 
